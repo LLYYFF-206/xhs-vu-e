1da521d77e9c8228783f74d3a34882af0c12f732
LLYYFF-206 <lg11090303@gmail.com>
2026-05-18 13:29:04 +0800

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -12,7 +12,7 @@ const jsonParser = (text) => {
 
 const api = axios.create({
   // ✅ 这里改成你正确的域名！
-  baseURL: 'https://collins-mph-dive-arc.xxx.com/api',
+baseURL: 'https://collins-mph-dive-architects.trycloudflare.com/api'
   timeout: 60000,
   // 自定义响应解析器，防止大整数精度丢失
   transformResponse: [(data) => {
